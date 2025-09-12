721ddacd06933c61eced7c404c648098a7e6ddad
Ana Flávia V. Kunzendorff <142523426+akunzendorff@users.noreply.github.com>
2025-09-12 17:53:15 -0300

    update nomenclatura model sensor

--- API/app/models/sensor.py
+++ API/app/models/sensor.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Float, ForeignKey
 from db.database import Base
 
 class Sensor(Base):
-    __tablename__ = 'sensors'
+    __tablename__ = 'sensor_readings'
 
     id = Column(Integer, primary_key=True)
     hive_id = Column(Integer, ForeignKey('hives.id'), nullable = False)
